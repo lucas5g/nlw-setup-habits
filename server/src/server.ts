b18d30cac13a453a791678769f32dd0d56e0bf47
lucas5g <lucasdesousa19@hotmail.com>
2023-01-20 00:53:39 -0300

    add port

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,15 @@
 import fastify from "fastify";
+
 import cors from '@fastify/cors'
 import { routes } from "./routes";
 
 const app = fastify()
 
 app.register(cors)
-app.register(routes)
-
+app.register(routes) 
 
 app.listen({
-  port:8000
+  port:Number(process.env.PORT) ?? 8000
 }).then(() => {
-  console.log('HTTP Server running!')
+  console.log(`HTTP Server running ${process.env.PORT}! `)
 })
